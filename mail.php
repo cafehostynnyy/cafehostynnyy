542efa1891e6a215be082066532f751817f2fc5c
hostynnyy <70815608+hostynnyy@users.noreply.github.com>
2020-09-05 21:20:12 +0300

    cafe

---
+++ mail.php
@@ -0,0 +1,39 @@
+<?php 
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'userr.cafe@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = '0979546450'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('userr.cafe@gmail.com'); // от кого будет уходить письмо?
+$mail->addAddress('cafedvir@gmail.com');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заказ кафе с сайта';
+$mail->Body    = '' .$name . ' оставил(a) заявку, его телефон ' .$phone . ' его заявка ' .$text ;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+     echo "Сообщение успешно отправлено";
+}
+?>
